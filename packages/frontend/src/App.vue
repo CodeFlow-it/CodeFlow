e8d583db201bda547296c87e0260d3f3a8f1e614
lmazeau <luky974@gmail.com>
2024-01-12 14:17:28 +0100

    le vide intersideral

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/auth">Auth</router-link>
   </nav>
   <router-view />
 </template>
@@ -28,3 +27,4 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script setup lang="ts"></script>
